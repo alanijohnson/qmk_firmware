51e4eba728d36e0f7885d0a1761ebcc2eff3f3c4
alanijohnson <alanijjohnson@gmail.com>
2023-02-12 14:17:28 -0600

    send layer and mods to non-master

--- keyboards/splitkb/kyria/keymaps/lani/keymap.c
+++ keyboards/splitkb/kyria/keymaps/lani/keymap.c
@@ -99,7 +99,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_
 
 bool oled_task_user(void) {
     
-    if (is_keyboard_master()) {
+    if (!is_keyboard_master()) {
         uint8_t mod_state = get_mods();
         const int row = 0;
 
