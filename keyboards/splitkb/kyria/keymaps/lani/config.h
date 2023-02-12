78337c7f7e1c8f151652cd284f834b186022c9c1
alanijohnson <alanijjohnson@gmail.com>
2023-02-12 14:20:32 -0600

    pass led state to non-master

--- keyboards/splitkb/kyria/keymaps/lani/config.h
+++ keyboards/splitkb/kyria/keymaps/lani/config.h
@@ -33,7 +33,7 @@
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
 #define SPLIT_LAYER_STATE_ENABLE
-// #define SPLIT_LED_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
 #define SPLIT_MODS_ENABLE
 // #define SPLIT_POINTING_ENABLE // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_pointing_device.md?id=split-keyboard-configuration
 
